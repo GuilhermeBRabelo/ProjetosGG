b252798281b33dbe380b0f632ef460b1d2e490e0
GuilhermeBRabelo <guilherme.rabelo08@aluno.ifce.edu.br>
2024-12-11 17:47:14 -0300

    Add files via upload

---
+++ projeto10 - JavaScript/js/script.js
@@ -0,0 +1,14 @@
+function mostrar() {
+    var ipt;
+    ipt = document.getElementById("iptX");
+    ipt = document.getElementById("iptY");
+    var x;
+    var y;
+
+    x = iptX.value;
+    y = iptY.value;
+
+    x = x + y;
+
+    alert(x);
+}
