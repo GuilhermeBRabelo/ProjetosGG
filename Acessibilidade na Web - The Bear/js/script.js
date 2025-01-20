1b24cc57050d2c2944ccaac72b28d25cc8d1c052
GuilhermeBRabelo <guilherme.rabelo08@aluno.ifce.edu.br>
2025-01-20 09:04:51 -0300

    Add files via upload

--- Acessibilidade na Web - The Bear/js/script.js
+++ Acessibilidade na Web - The Bear/js/script.js
@@ -1,3 +1,17 @@
-function disponibilidadeMesas() {
-    
+function alternarMesa(elemento) {
+    // Verifica o status atual da mesa
+    const status = elemento.getAttribute('data-status');
+
+    // Se a mesa estiver ocupada, muda para disponível
+    if (status === 'ocupado') {
+        elemento.classList.remove('ocupado');
+        elemento.classList.add('disponivel');
+        elemento.setAttribute('data-status', 'disponivel'); // Atualiza o status da mesa
+    } 
+    // Se a mesa estiver disponível, muda para ocupado
+    else if (status === 'disponivel') {
+        elemento.classList.remove('disponivel');
+        elemento.classList.add('ocupado');
+        elemento.setAttribute('data-status', 'ocupado'); // Atualiza o status da mesa
+    }
 }
